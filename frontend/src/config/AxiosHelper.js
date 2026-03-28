81f1fdc234ffda94008742c16e2f21d734bcc7e4
Mohit4321-be <heerupokhariya@gmail.com>
2026-03-28 22:39:19 +0530

    axios api changed

--- frontend/src/config/AxiosHelper.js
+++ frontend/src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const baseURL = 'http://localhost:8080';
+export const baseURL = 'https://chatappbackend-07aq.onrender.com';
 export const httpClient = axios.create({
     baseURL: baseURL
 });
